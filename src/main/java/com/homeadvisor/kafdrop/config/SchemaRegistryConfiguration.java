9439427a7dc765a426119dc27af193725e738cb7
Luc Cary (CU) <luc.cary@gmail.com>
2018-11-14 11:33:31 -0500

    Revert "Remove list-parsing code from schema registry config parser"
    This reverts commit 46dfcdbc53671c78f37a74fa23edf2471bd9405a.

--- src/main/java/com/homeadvisor/kafdrop/config/SchemaRegistryConfiguration.java
+++ src/main/java/com/homeadvisor/kafdrop/config/SchemaRegistryConfiguration.java
@@ -4,6 +4,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 @Configuration
 public class SchemaRegistryConfiguration {
@@ -12,6 +15,9 @@ public class SchemaRegistryConfiguration {
     @ConfigurationProperties(prefix = "schemaregistry")
     public static class SchemaRegistryProperties
     {
+
+        public static final Pattern CONNECT_SEPARATOR = Pattern.compile("\\s*,\\s*");
+
         private String connect;
 
         public String getConnect()
@@ -22,6 +28,14 @@ public class SchemaRegistryConfiguration {
         public void setConnect(String connect)
         {
             this.connect = connect;
+        }
+
+        public List<String> getConnectList()
+        {
+            return CONNECT_SEPARATOR.splitAsStream(this.connect)
+                    .map(String::trim)
+                    .filter(s -> s.length() > 0)
+                    .collect(Collectors.toList());
         }
 
     }
