e4d8e9e104109eefbcb89e8a7e8596ab2a483dab
Emil Koutanov <ekoutanov@gmail.com>
2019-06-16 10:01:39 +1000

    Conditional loading of BuildInfo if build-info.properties is present

--- src/main/java/kafdrop/service/BuildInfo.java
+++ src/main/java/kafdrop/service/BuildInfo.java
@@ -1,10 +1,12 @@
 package kafdrop.service;
 
 import org.slf4j.*;
+import org.springframework.boot.autoconfigure.condition.*;
 import org.springframework.boot.info.*;
 import org.springframework.stereotype.*;
 
 @Service
+@ConditionalOnResource(resources="${spring.info.build.location:classpath:META-INF/build-info.properties}")
 public final class BuildInfo {
   public BuildInfo(BuildProperties buildProperties) {
     final var log = LoggerFactory.getLogger(BuildInfo.class);
