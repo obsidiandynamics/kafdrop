b46c0ca7b66c4d5973ded932a3417c049dba9d8f
Emil Koutanov <ekoutanov@gmail.com>
2019-06-29 22:51:36 +1000

    Debug

--- ci-script.sh
+++ ci-script.sh
@@ -32,6 +32,7 @@ if [[ ! $app_ver =~ "-SNAPSHOT" ]]; then
   fi
 
   if [ $GITHUB_RELEASE_ENABLED = 1 ]; then
+    curl -s -H 'User-Agent: CI' $repo_url/releases/tags/$app_ver
     get_release_tag=$(curl -s -o /dev/null -w "%{http_code}" -H 'User-Agent: CI' $repo_url/releases/tags/$app_ver)
     echo "Release tag $get_release_tag"
     if [ $get_release_tag == "404" ]; then
