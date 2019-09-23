9b5ae853804600fdf253f910d899ad7f1278c992
Emil Koutanov <ekoutanov@gmail.com>
2019-09-23 18:33:14 +1000

    Canonical location of kafka.properties and kafka.truststore.jks

--- src/main/java/kafdrop/config/KafkaConfiguration.java
+++ src/main/java/kafdrop/config/KafkaConfiguration.java
@@ -3,6 +3,7 @@ package kafdrop.config;
 import lombok.*;
 import org.apache.kafka.clients.*;
 import org.apache.kafka.common.config.*;
+import org.slf4j.*;
 import org.springframework.boot.context.properties.*;
 import org.springframework.stereotype.*;
 
@@ -13,13 +14,16 @@ import java.util.*;
 @ConfigurationProperties(prefix = "kafka")
 @Data
 public final class KafkaConfiguration {
+  private static final Logger LOG = LoggerFactory.getLogger(KafkaConfiguration.class);
+
+  private static final String KAFKA_PROPERTIES_FILE = "kafka.properties";
+
+  private static final String KAFKA_TRUSTSTORE_FILE = "kafka.truststore.jks";
+
   private String brokerConnect;
   private Boolean isSecured = false;
   private String saslMechanism;
   private String securityProtocol;
-
-  /** Extra properties, base-64 encoded. Will override any prior properties. */
-  private String props;
 
   public void applyCommon(Properties properties) {
     properties.setProperty(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, brokerConnect);
@@ -28,15 +32,21 @@ public final class KafkaConfiguration {
       properties.put(SaslConfigs.SASL_MECHANISM, saslMechanism);
     }
 
-    if (props != null) {
-      final var propsDecoded = new String(Base64.getDecoder().decode(props));
-      final var extraProps = new Properties();
-      try (var propsReader = new StringReader(propsDecoded)) {
-        extraProps.load(propsReader);
+    if (new File(KAFKA_TRUSTSTORE_FILE).isFile()) {
+      LOG.info("Assigning truststore location to {}", KAFKA_TRUSTSTORE_FILE);
+      properties.put("ssl.truststore.location", KAFKA_TRUSTSTORE_FILE);
+    }
+
+    final var propertiesFile = new File(KAFKA_PROPERTIES_FILE);
+    if (propertiesFile.isFile()) {
+      LOG.info("Loading properties from {}", KAFKA_PROPERTIES_FILE);
+      final var propertyOverrides = new Properties();
+      try (var propsReader = new BufferedReader(new FileReader(propertiesFile))) {
+        propertyOverrides.load(propsReader);
       } catch (IOException e) {
         throw new KafkaConfigurationException(e);
       }
-      properties.putAll(extraProps);
+      properties.putAll(propertyOverrides);
     }
   }
 }
