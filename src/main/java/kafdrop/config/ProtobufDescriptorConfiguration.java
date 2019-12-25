81b8795e853cd06dea83f232994db8c5ff03b692
dchu <dchu@etcc.com>
2019-12-25 16:20:44 -0600

    Fix issue when using without a protobuf desc folder configured.

--- src/main/java/kafdrop/config/ProtobufDescriptorConfiguration.java
+++ src/main/java/kafdrop/config/ProtobufDescriptorConfiguration.java
@@ -2,9 +2,14 @@ package kafdrop.config;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
@@ -14,6 +19,7 @@ public class ProtobufDescriptorConfiguration {
 	@Component
 	@ConfigurationProperties(prefix = "protobufdesc")
 	public static final class ProtobufDescriptorProperties {
+		private static final Logger LOG = LoggerFactory.getLogger(ProtobufDescriptorProperties.class);
 		//the idea is to let user specifying a directory stored all descriptor file
 		//the program will load and .desc file and show as an option on the message detail screen 
 		private String directory;
@@ -28,6 +34,10 @@ public class ProtobufDescriptorConfiguration {
 		
 		public List<String> getDescFilesList(){
 			//getting file list
+			if(directory==null || Files.notExists(Path.of(directory))) {
+				LOG.error("No descriptor folder configured, skip the setting!!");
+				return Collections.emptyList();
+			}
 			String[] pathnames;
 			File path = new File(directory);
 			
