c364258d7f1d71b858394dea6ef7d4be00a489f3
Varun <varun.wachaspati@bloomreach.com>
2020-08-05 12:13:32 +0530

    Add MessagePack Support
    Can deserialize msgpack messages on kafka topic and outputs JSON
    String.
    Using the official msgPack java client v0.8.20.
    Message is collapsed by default.
    Tested against sample msgpack payloads, formatting looks fine.

---
+++ src/main/java/kafdrop/util/MsgPackMessageDeserializer.java
@@ -0,0 +1,27 @@
+package kafdrop.util;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.msgpack.core.MessagePack;
+import org.msgpack.core.MessageUnpacker;
+
+public class MsgPackMessageDeserializer implements MessageDeserializer {
+
+  private static final Logger LOG = LoggerFactory.getLogger(MsgPackMessageDeserializer.class);
+
+  @Override
+  public String deserializeMessage(ByteBuffer buffer) {
+    MessageUnpacker unpacker = MessagePack.newDefaultUnpacker(buffer);
+    try {
+      return unpacker.unpackValue().toJson();
+    } catch (IOException e) {
+      final String errorMsg = "Unable to unpack msgpack message";
+      LOG.error(errorMsg, e);
+      throw new DeserializationException(errorMsg);
+    }
+  }
+}
