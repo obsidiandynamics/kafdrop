7c9c7045780693bc411306c23dcfe46705adf2c0
shubham <shubhamgulati81@mail.com>
2021-01-31 05:45:36 -0800

    added new UI to publish payload on any topic

---
+++ src/main/java/kafdrop/controller/PublisherController.java
@@ -0,0 +1,59 @@
+package kafdrop.controller;
+
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import kafdrop.model.PublishPayloadVO;
+import kafdrop.model.TopicVO;
+import kafdrop.service.KafkaHighLevelProducer;
+import kafdrop.service.KafkaMonitor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/publish")
+public class PublisherController {
+
+    @Autowired
+    KafkaHighLevelProducer producer;
+    @Autowired
+    KafkaMonitor kafkaMonitor;
+
+    @RequestMapping("/ui")
+    public String consumerDetail(Model model) {
+        model.addAttribute("topicNames",kafkaMonitor.getTopics().stream().map(TopicVO::getName).toArray());
+        return "publish-payload";
+    }
+
+    @PostMapping("{topic}")
+    public void publish(@PathVariable("topic") String producerTopics, @RequestBody String message){
+        try{
+            producer.publish(producerTopics,message);
+        }catch(Exception e){System.out.println(e);}
+        System.out.println("Success...");
+    }
+
+
+    @ApiOperation(value = "publishPayload", notes = "publish payload")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success", response = String.class)
+    })
+    @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
+    public String publishTopic(PublishPayloadVO publishPayloadVO, Model model) {
+
+        try{
+            model.addAttribute("topicName", publishPayloadVO.getName());
+            producer.publish(publishPayloadVO.getName(),publishPayloadVO.getPayload());
+
+        }catch(Exception e){
+            model.addAttribute("errorMessage", e.getMessage());
+            System.out.println(e);}
+        System.out.println("Success...");
+        model.addAttribute("published", true);
+        return "publish-payload";
+    }
+}
