e4496fc9f70a67e8142aad266515a2c9c3cfbb02
Harshil Nori <harshilnori@Harshils-MacBook-Air.local>
2021-06-15 11:49:12 +0530

    Added support for iam enabled msk using custom role attached to k8s pod

--- src/main/java/kafdrop/config/KafkaConfiguration.java
+++ src/main/java/kafdrop/config/KafkaConfiguration.java
@@ -11,31 +11,50 @@ import org.springframework.stereotype.*;
 
 
 @Component
-@ConfigurationProperties(prefix = "kafka")
+@ConfigurationProperties (prefix = "kafka")
 @Data
 public final class KafkaConfiguration {
   private static final Logger LOG = LoggerFactory.getLogger(KafkaConfiguration.class);
 
   private String brokerConnect;
+
   private Boolean isSecured = false;
+
   private String saslMechanism;
+
   private String securityProtocol;
+
   private String truststoreFile;
+
   private String propertiesFile;
+
   private String keystoreFile;
+
+  private String jaasConfig;
+
+  private String clientCallback;
+
+  private String iamEnabled;
 
   public void applyCommon(Properties properties) {
     properties.setProperty(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, brokerConnect);
-    if (isSecured) {
-      LOG.warn("The 'isSecured' property is deprecated; consult README.md on the preferred way to configure security");
-      properties.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, securityProtocol);
-      properties.put(SaslConfigs.SASL_MECHANISM, saslMechanism);
-    }
+
+    LOG.info("Setting security protocol to {}", securityProtocol);
+    LOG.info("Setting sasl mechanism to {}", saslMechanism);
+    properties.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, securityProtocol);
+    properties.put(SaslConfigs.SASL_MECHANISM, saslMechanism);
 
     LOG.info("Checking truststore file {}", truststoreFile);
     if (new File(truststoreFile).isFile()) {
       LOG.info("Assigning truststore location to {}", truststoreFile);
       properties.put("ssl.truststore.location", truststoreFile);
+    }
+
+    LOG.info("Setting iam enabled {}", iamEnabled);
+    if (Boolean.parseBoolean(iamEnabled)) {
+      LOG.info("Setting sasl.jaas.config {} and sasl and callback callback properties {}", jaasConfig, clientCallback);
+      properties.put(SaslConfigs.SASL_CLIENT_CALLBACK_HANDLER_CLASS, clientCallback);
+      properties.put(SaslConfigs.SASL_JAAS_CONFIG, jaasConfig);
     }
 
     LOG.info("Checking keystore file {}", keystoreFile);
