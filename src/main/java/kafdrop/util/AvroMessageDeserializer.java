ad8aa0f12530b44be02c1264393283b48eb2f5ef
Yevgeniy Magdel <yevgeniy.magdel@rallyhealth.com>
2021-11-17 12:53:57 -0600

    [allow_configuring_avro_consumer] Read avro consumer configs from env var

--- src/main/java/kafdrop/util/AvroMessageDeserializer.java
+++ src/main/java/kafdrop/util/AvroMessageDeserializer.java
@@ -4,6 +4,7 @@ import io.confluent.kafka.serializers.*;
 
 import java.nio.*;
 import java.util.*;
+import java.util.stream.Collectors;
 
 
 public final class AvroMessageDeserializer implements MessageDeserializer {
@@ -12,7 +13,7 @@ public final class AvroMessageDeserializer implements MessageDeserializer {
 
   public AvroMessageDeserializer(String topicName, String schemaRegistryUrl, String schemaRegistryAuth) {
     this.topicName = topicName;
-    this.deserializer = getDeserializer(schemaRegistryUrl, schemaRegistryAuth);
+    this.deserializer = getDeserializer(schemaRegistryUrl, schemaRegistryAuth, topicName);
   }
 
   @Override
@@ -22,15 +23,33 @@ public final class AvroMessageDeserializer implements MessageDeserializer {
     return deserializer.deserialize(topicName, bytes).toString();
   }
 
-  private static KafkaAvroDeserializer getDeserializer(String schemaRegistryUrl, String schemaRegistryAuth) {
+  private static KafkaAvroDeserializer getDeserializer(String schemaRegistryUrl, String schemaRegistryAuth, String topicName) {
     final var config = new HashMap<String, Object>();
     config.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl);
     if (schemaRegistryAuth != null) {
       config.put(AbstractKafkaSchemaSerDeConfig.BASIC_AUTH_CREDENTIALS_SOURCE, "USER_INFO");
       config.put(AbstractKafkaSchemaSerDeConfig.USER_INFO_CONFIG, schemaRegistryAuth);
     }
+    setConfigFromEnvIfAvailable(topicName, AbstractKafkaAvroSerDeConfig.VALUE_SUBJECT_NAME_STRATEGY, config);
     final var kafkaAvroDeserializer = new KafkaAvroDeserializer();
     kafkaAvroDeserializer.configure(config, false);
     return kafkaAvroDeserializer;
+  }
+
+  private static void setConfigFromEnvIfAvailable(String topicName, String configPath, Map<String,Object> config){
+
+    String configPrefix = "SCHEMA_REGISTRY";
+    String topicScopedEnvPath = Arrays.stream(new String[]{configPrefix, configPath.replace(".", "_"), topicName.replace("-", "_") } )
+            .map(String::toUpperCase).collect(Collectors.joining("_"));
+
+    String noTopicScopedEnvPath = Arrays.stream(new String[]{ "SCHEMA_REGISTRY", configPath.replace(".", "_") })
+            .map(String::toUpperCase).collect(Collectors.joining("_"));
+
+    for(String envPath : new String[]{topicScopedEnvPath, noTopicScopedEnvPath}) {
+      String namingStrategyValue = System.getenv(envPath);
+      if (namingStrategyValue != null) {
+        config.put(envPath, namingStrategyValue);
+      }
+    }
   }
 }
