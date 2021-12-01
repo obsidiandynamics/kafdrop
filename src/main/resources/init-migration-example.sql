1fa66a79ec83d7b66e03ec4d3798ba274d6d5ff9
Uri Shamay <cmpxchg16@gmail.com>
2021-12-01 17:16:45 +0200

    Add native authentication

--- src/main/resources/init-migration-example.sql
+++ src/main/resources/init-migration-example.sql
@@ -1,6 +1,6 @@
 -- this is just an example, if you want to use it as is, you "just" need to crack the secured bcrypt string
 INSERT INTO users (username, password, enabled)
-VALUES ('f95a2ec06fc04e72','$2a$10$MvbTMKGWeBNBeCXKqFo/we0da4ai17IlQ7lq4nWzd3rQAQjR9COIS',1);
+VALUES ('<REPLACEME>','<REPLACEME>',1);
 
 INSERT INTO authorities (username, authority)
-VALUES ('f95a2ec06fc04e72','ROLE_ADMIN'); -- ROLE_USER
+VALUES ('<REPLACEME>','ROLE_ADMIN'); -- ROLE_USER
