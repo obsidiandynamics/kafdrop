c4cc4f7063387c26f07613ef8632a43367cead42
Marcus Krassmann <marcus.krassmann@gmail.com>
2021-12-19 22:28:11 +0100

    update to spring boot 2.5.7

--- src/main/java/kafdrop/controller/BasicErrorController.java
+++ src/main/java/kafdrop/controller/BasicErrorController.java
@@ -1,6 +1,7 @@
 package kafdrop.controller;
 
 import org.springframework.boot.autoconfigure.web.servlet.error.*;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.servlet.error.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
@@ -17,14 +18,14 @@ public final class BasicErrorController extends AbstractErrorController {
 
   @RequestMapping("/error")
   public ModelAndView handleError(HttpServletRequest request) {
-    final var error = getErrorAttributes(request, true);
+    final var error = getErrorAttributes(request, ErrorAttributeOptions.of(ErrorAttributeOptions.Include.STACK_TRACE));
     System.out.println("errorAtts: " + error);
     final var model = Map.of("error", error);
     return new ModelAndView("error", model);
   }
 
-  @Override
   public String getErrorPath() {
-    return "/error";
+    // not used anymore (see javadoc, remove method when spring boot removes it from ErrorController interface)
+    return null;
   }
 }
