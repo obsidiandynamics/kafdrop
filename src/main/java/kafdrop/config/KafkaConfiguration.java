d5fea142928f281ff85613ab23d424f97174a7a1
krishna-prasad-s <skrishnaprasad@gmail.com>
2022-02-05 20:37:51 +0530

    move security protocol out of isSecured block (#202)

--- src/main/java/kafdrop/config/KafkaConfiguration.java
+++ src/main/java/kafdrop/config/KafkaConfiguration.java
@@ -28,8 +28,11 @@ public final class KafkaConfiguration {
     properties.setProperty(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, brokerConnect);
     if (isSecured) {
       LOG.warn("The 'isSecured' property is deprecated; consult README.md on the preferred way to configure security");
-      properties.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, securityProtocol);
       properties.put(SaslConfigs.SASL_MECHANISM, saslMechanism);
+    }
+
+    if(isSecured || securityProtocol.equals("SSL")) {
+      properties.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, securityProtocol);
     }
 
     LOG.info("Checking truststore file {}", truststoreFile);
