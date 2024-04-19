06ad7ca3ba5a0c62b86285af3e6e8009dbc2bffb
Alvin Kwekel <am.kwekel@minvws.nl>
2024-04-19 15:27:46 +0200

    Add support for loading kafka properties from classpath

--- src/main/java/kafdrop/config/KafkaConfiguration.java
+++ src/main/java/kafdrop/config/KafkaConfiguration.java
@@ -1,17 +1,19 @@
 package kafdrop.config;
 
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.clients.CommonClientConfigs;
-import org.apache.kafka.common.config.SaslConfigs;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 
@@ -48,16 +50,26 @@ public final class KafkaConfiguration {
     }
 
     LOG.info("Checking properties file {}", propertiesFile);
-    final var propertiesFile = new File(this.propertiesFile);
-    if (propertiesFile.isFile()) {
-      LOG.info("Loading properties from {}", this.propertiesFile);
-      final var propertyOverrides = new Properties();
-      try (var propsReader = new BufferedReader(new FileReader(propertiesFile))) {
-        propertyOverrides.load(propsReader);
+    if (StringUtils.isNotBlank(propertiesFile)) {
+      InputStream inputStream;
+      try {
+        Resource fileSystemResource = new FileSystemResource(this.propertiesFile);
+        Resource classPathResource = new ClassPathResource(this.propertiesFile);
+        if (fileSystemResource.isReadable()) {
+          inputStream = fileSystemResource.getInputStream();
+        } else if (classPathResource.isReadable()) {
+          inputStream = classPathResource.getInputStream();
+        } else {
+          throw new KafkaConfigurationException(new IllegalArgumentException(
+            "Kafka properties file %s is not valid or can not be found".formatted(this.propertiesFile)));
+        }
+        LOG.info("Loading properties from {}", this.propertiesFile);
+        final var propertyOverrides = new Properties();
+        propertyOverrides.load(inputStream);
+        properties.putAll(propertyOverrides);
       } catch (IOException e) {
         throw new KafkaConfigurationException(e);
       }
-      properties.putAll(propertyOverrides);
     }
   }
 }
